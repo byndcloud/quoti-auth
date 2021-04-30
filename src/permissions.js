568200d4992952aece750a0e3426996890cf7009
Davi Moura <davi.dev@icloud.com>
2021-04-30 11:35:30 -0300

    chore: last release

---
+++ src/permissions.js
@@ -0,0 +1,141 @@
+const uniqBy = require('lodash/uniqBy')
+
+/**
+ * @description Get permissions for a organization granted to organizational user
+ * @param {String} slug Organization's slug to get permissions
+ */
+const getOrganizationalUserOrganizationPermissions = logger => {
+  return (slug, permissions) => {
+    const organizationPermissions = []
+    try {
+    // For each api key environment
+      for (const apiKeyEnvironment in permissions) {
+        logger.verbose('Api key environment:', { apiKeyEnvironment })
+
+        logger.verbose('Granted permissions are:', permissions[apiKeyEnvironment])
+
+        // If environment is a regular expression
+        if (apiKeyEnvironment.startsWith('/') && apiKeyEnvironment.endsWith('/')) {
+          logger.verbose('Will try to match with', { slug })
+
+          // If matchs with organization slug, push permissions (will match /.*/ with any or no organization)
+          if (slug.match(new RegExp(apiKeyEnvironment.slice(1, -1), 'g'))) {
+            logger.verbose('It\'s a match! Pushing permissions')
+
+            organizationPermissions.push(...permissions[apiKeyEnvironment])
+          }
+        } else {
+          logger.verbose('Will try to compare with', { slug })
+
+          // If it's a simple string, and is equal to the organization slug, push permissions
+          if (apiKeyEnvironment === slug) {
+            logger.verbose('Sucess! Pushing permissions', { slug })
+
+            organizationPermissions.push(...permissions[apiKeyEnvironment])
+          }
+        }
+      }
+    } catch (err) {
+      logger.error(err)
+    }
+
+    return organizationPermissions
+  }
+}
+
+/**
+ * @description Validate permissions
+ * This function receives an array of validators, and tries to validate some of them, i.e.,
+ * each validator can be an array or a regular expression. When it's an array, the
+ * validator requires the user to have all of its permissions. When it's a regular
+ * expression, the validator requires some user permission to match it.
+ * @param {Array.<Array.<String>|RegExp>} validators Validators
+ */
+function validateSomePermissionCluster (logger) {
+  return (validators = []) => {
+    return (req, res) => {
+      if (!req.user) {
+        return null
+      }
+      logger.profile('MiddlewarePermissions', { level: 'verbose' })
+
+      logger.log('Requiring access', { url: req.originalUrl })
+
+      logger.log('Validators are', [validators])
+
+      // Pass test if no permission required
+      if (validators?.length === 0) {
+        logger.log('User passed permission test')
+        return true
+      }
+
+      logger.log('Getting users permissions')
+
+      // Get user permissions
+
+      // If auth method is api key
+      if (req.get('ApiKey')) {
+        const orgSlug = req.params.orgSlug || ''
+
+        const apiKeyPermissions = getOrganizationalUserOrganizationPermissions(logger)(orgSlug, req.user.permissions)
+
+        // Push api key permissions to user permissions
+        if (req.user.Permissions) {
+          req.user.Permissions.push(...apiKeyPermissions)
+        } else {
+          req.user.Permissions = apiKeyPermissions
+        }
+      }
+
+      const userPermissions = uniqBy(req.user.Permissions, 'name')
+      req.permissions = {
+        validated: []
+      }
+
+      for (const validator of validators) {
+        if (validator instanceof Array) {
+          // Get the intersection between users permissions and validator permissions
+          const intersection = userPermissions.filter(userPermission => validator.includes(userPermission.name))
+
+          // If the intersection is the validator itself, then the user has all the validator's permissions
+          if (intersection.length === validator.length) {
+            req.permissions.validated.push({
+              by: 'intersection',
+              intersection
+            })
+          }
+        } else if (validator instanceof RegExp) {
+          // Try to match the user permissions with the validator
+          const match = []
+
+          for (const userPermission of userPermissions) {
+            if (validator.test(userPermission.name)) {
+              match.push(userPermission)
+            }
+          }
+
+          // If some permission is validated, then the user matchs the validator
+          if (match.length > 0) {
+            req.permissions.validated.push({
+              by: 'expression',
+              expression: validator,
+              match
+            })
+          }
+        }
+      }
+
+      // If some validator were validated, pass test
+      if (req.permissions.validated.length > 0) {
+        logger.log('User passed permission test')
+        return true
+      } else {
+        const error = new Error(`Insufficient permissions! Permissions ${validators.join(', ')} are required`)
+        logger.error(error)
+        res.status(401).send(error)
+      }
+    }
+  }
+}
+
+module.exports = { validateSomePermissionCluster, getOrganizationalUserOrganizationPermissions }
---
+++ src/quotiauth.js
@@ -0,0 +1,72 @@
+
+const axios = require('axios')
+const Permissions = require('./permissions')
+class QuotiAuth {
+  constructor (orgSlug, apiKey, getUserData, logger) {
+    this.orgSlug = orgSlug
+    this.apiKey = apiKey
+    this.logger = logger
+    if (getUserData) {
+      this.getUserData = getUserData
+    }
+  }
+
+  async getUserData (token) {
+    const url = process.env['api_url'] || 'http://api.minhafaculdade.app/api/v1/'
+    const headers = {
+      ApiKey: this.apiKey
+    }
+    const { data } = await axios.post(`${url}${this.orgSlug}/auth/login/getuser`, { token }, { headers })
+    return data?.user
+  }
+
+  setup ({ orgSlug, apiKey, getUserData, logger }) {
+    this.orgSlug = orgSlug
+    this.apiKey = apiKey
+    this.logger = logger || console
+    if (getUserData) {
+      this.getUserData = getUserData
+    }
+  }
+
+  validateSomePermissionCluster (args) {
+    return Permissions.validateSomePermissionCluster(this.logger)(args)
+  }
+
+  /**
+   * @description Sets req.user
+   * @param {import('express').Request} req
+   * @param {import('express').Response} res
+   * @param {import('express').NextFunction} next
+   */
+  middleware (permissions = null) {
+    return async (req, res, next) => {
+      try {
+        if (!req?.body) {
+          throw new Error('You shold have a body parser in your express to parse the body of request.')
+        }
+        if (!req?.body?.token) {
+          throw new Error('You shold have send a token in the body of request to search.')
+        }
+        const token = req?.body?.token
+
+        const result = await this.getUserData(token)
+        req.user = result
+        if (permissions) {
+          const permissionsResult = this.validateSomePermissionCluster(permissions)(req, res)
+          if (!permissionsResult) {
+            throw new Error('Insufficient permissions or user is null')
+          }
+        }
+
+        next()
+      } catch (err) {
+        console.error(err)
+        res.status(500).send('Error! See console for more.')
+      }
+      return null
+    }
+  }
+}
+
+module.exports = new QuotiAuth()
