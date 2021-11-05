7a6edf29b12956cffb79ad7e785ea479f46257d1
Luiz Albuquerque <laaj@cin.ufpe.br>
2021-11-05 17:34:17 -0300

    feat: adding index.d.ts. Putting Typescript on project

---
+++ index.d.ts
@@ -0,0 +1,92 @@
+import { Request, Response, NextFunction } from 'express';
+
+declare module 'quoti-auth' {
+  export const quotiAuth = new QuotiAuth();
+}
+
+// todo: Obtive esse type analisando diretamente o log quando executamos a função getUserData. É isso mesmo?
+type UserData = Partial<{
+  id: number;
+  uuid: string;
+  name: string;
+  gender: string;
+  user: string;
+  pass: string;
+  passwordStrength: string;
+  cpf: string | number;
+  email: string;
+  telefones: string | null;
+  matricula: string | null;
+  ppessoaCodigo: null;
+  token: string;
+  tokenPasswordReset: string;
+  tokenPasswordResetExpirationTime: string;
+  tokenPasswordResetTimes: string;
+  registered: boolean;
+  lastLoginTime: string;
+  userCreated: string | boolean;
+  createdTime: string;
+  updatedTime: string;
+  userProfileId: number;
+  sync: boolean;
+  bio: string;
+  birthday: string;
+  instagramUrl: string;
+  facebookUrl: string;
+  twitterUrl: string;
+  linkedinUrl: string;
+  expiresAt: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: string;
+  user_profile_id: number;
+  media_id: number;
+  image: string;
+  UsersProfile: { id: number; homePageDefault: string };
+  Groups: { id: number; name: string }[];
+  Permissions: { id: number; name: string }[];
+}>;
+
+type QuotiPermissions = string[][];
+// todo: verificar o type logger. Provavelmente o logger do quoti não tem type console
+type SetupConfig = {
+  orgSlug?: string;
+  apiKey?: string;
+  getUserData?: unknown;
+  logger?: Console;
+};
+
+type ValidateSomePermissionClusterMiddleware = (
+  logger,
+) => ValidateSomePermissionClusterMiddlewareAux;
+type ValidateSomePermissionClusterMiddlewareAux = (
+  validators: string[],
+) => ValidateSomePermissionClusterMiddlewareAux2;
+type ValidateSomePermissionClusterMiddlewareAux2 = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => boolean | null | Response;
+
+class QuotiAuth {
+  constructor(setupConfig: SetupConfig): void;
+
+  middleware(
+    permissions: QuotiPermissions,
+  ): (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => null | void | Response;
+
+  setup(setupConfig: SetupConfig): void;
+
+  getUserData(data: {
+    token: string;
+    orgSlug: string;
+  }): Promise<UserData | unknown>;
+
+  validateSomePermissionCluster(
+    permissions: QuotiPermissions,
+  ): ValidateSomePermissionClusterMiddlewareAux2;
+}
