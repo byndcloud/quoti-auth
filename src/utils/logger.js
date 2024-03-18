14566481376569b3f86093b3a678d73a22ab72d8
Victor Aurelio <ovictoraurelio@gmail.com>
2024-03-18 16:17:10 -0300

    feat: add logger utility functions

---
+++ src/utils/logger.js
@@ -0,0 +1,40 @@
+module.exports.parseAxiosError = function (error) {
+  let errorMessage = `Error Message: \n  ${error.message}\n\n`;
+
+  if (error?.request) {
+    errorMessage += `Request: \n  ${error.request.method.toUpperCase()} ${error.request.url}\n\n`;
+
+    if (error?.request?.data) {
+      errorMessage += `Request Data: \n  ${JSON.stringify(error.request.data, null, 2)}\n\n`;
+    }
+  }
+
+  if (error?.response) {
+    errorMessage += `Response: \n  ${error.response.status} ${error.response.statusText}\n\n`;
+
+    if (error?.response?.data) {
+      errorMessage += `Response Data: \n  ${JSON.stringify(error.response.data, null, 2)}\n\n`;
+    }
+  }
+
+  return errorMessage;
+}
+
+module.exports.validateLogLevel = function(logLevel) {
+  const validLogLevels = [
+    'error',
+    'warn',
+    'info',
+    'verbose',
+    'debug',
+    'silly'
+  ]
+  if (validLogLevels.indexOf(logLevel) === -1) {
+    throw new Error('Invalid errorLogLevel level at QuotiAuth setup')
+  }
+
+  const isFunctionOfLogger = this.logger[logLevel] instanceof Function
+  if (!isFunctionOfLogger) {
+    throw new Error('Invalid logger/logLevel at QuotiAuth setup because it does not have the errorLogLevel method implemented at logger')
+  }
+}
