157a2a4d813b035523dbe219cc0f92139a120dab
Victor Aurelio <ovictoraurelio@gmail.com>
2024-03-18 16:14:50 -0300

    feat: add e2e setup file

---
+++ e2e/setup.mjs
@@ -0,0 +1,11 @@
+import chai from 'chai'
+import chaiHttp from 'chai-http'
+
+chai.use(chaiHttp)
+
+export const mochaHooks = {
+  afterAll() {
+    console.info('Finished execution of e2e tests')
+    process.exit(0)
+  }
+}
